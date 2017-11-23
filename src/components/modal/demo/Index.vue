2fb92f474a6bc13ac19c5f9272df573ee0b1ece0
liubo <2313018662@qq.com>
2017-11-23 18:00:28 +0800

    1

--- src/components/modal/demo/Index.vue
+++ src/components/modal/demo/Index.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="demo">
-    <i-button>modal</i-button>
-    <modal></modal>
+    <i-button>modal1234</i-button>
+    <!-- <modal></modal> -->
+    <div class="wh-modal-container">
+      <div class="wh-modal-mask"></div>
+      <div class="wh-modal-wrap">
+        <div class="wh-modal wh-modal-transparent">
+          <div class="wh-modal-content">
+            <div class="wh-modal-hd">
+              <div class="wh-modal-title">标题栏</div>
+            </div>
+            <div class="wh-modal-bd">
+              内容区，输入说明文字！
+            </div>
+            <div class="wh-modal-ft">
+              <!-- buttons -->
+              <div class="wh-modal-button-group-h" role="group">
+                <a class="wh-modal-button">取消</a>
+                <a class="wh-modal-button">确定</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +35,7 @@
 </script>
 
 <style lang="less" scoped>
+@import "../src/style/modal";
 .demo {
   padding: 20px;
 }
--- src/components/modal/src/Modal.vue
+++ src/components/modal/src/Modal.vue
@@ -2,12 +2,12 @@
   <div class="wh-modal-container">
     <div class="wh-modal-mask"></div>
     <div class="wh-modal-wrap">
-      <div class="wh-modal wh-modal-transparent1">
+      <div class="wh-modal wh-modal-transparent">
         <div class="wh-modal-content">
           <div class="wh-modal-hd">1</div>
-          <div class="wh-modal-bd">2</div>
+          <div class="wh-modal-bd">24</div>
           <div class="wh-modal-ft">
-            333
+            <!-- buttons -->
           </div>
         </div>
       </div>
@@ -18,20 +18,17 @@
 <script>
   // const prefixCls = 'wh-modal'
   export default {
-    name: 'Modal',
-    props: {
-      visible: {
-        type: Boolean,
-        default: false
-      },
-      maskClosable: {
-        type: Boolean,
-        default: true
-      }
-    },
-    computed: {
-
-    }
+    // name: 'Modal',
+    // props: {
+    //   visible: {
+    //     type: Boolean,
+    //     default: false
+    //   },
+    //   maskClosable: {
+    //     type: Boolean,
+    //     default: true
+    //   }
+    // }
   }
 </script>
 
