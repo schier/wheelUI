69b4d1f6f3c1e5870b88564df1010ac651f0dc7f
liubo <2313018662@qq.com>
2017-11-29 20:49:00 +0800

    1

--- src/components/popover/demo/Index.vue
+++ src/components/popover/demo/Index.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="demo">
     <h3>popover</h3>
-    <popover>文字</popover>
+    <div class="wh-popover-wrap">
+      <div class="wh-popover-mask"></div>
+      <div class="wh-popover-rel"><slot>按钮</slot></div>
+      <transition name="fade">
+        <div class="wh-popover">
+          <div class="wh-popover-arrow"></div>
+          <div class="wh-popover-inner">
+            <p style="padding:10px;">Poptip 气泡提示</p>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -17,7 +28,11 @@
 </script>
 
 <style lang="less" scoped>
+@import "../src/style/popover";
 .demo {
   padding: 20px;
+}
+h3 {
+  margin-bottom: 20px;
 }
 </style>
